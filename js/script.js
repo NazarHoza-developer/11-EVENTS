34330cee79f0798bebf3f798da938a74c07b60b1
nazarhoza <nazarhoza@gmail.com>
2022-09-21 19:06:06 +0300

    new

--- js/script.js
+++ js/script.js
@@ -3,18 +3,18 @@
 //  (клавіши можуть бути будь-які, звуки можуть бути будь-які, тільки
 // вкажіть у верстці – які кнопки натискати). В ідеалі повинно вийти як
 // по посиланню."
-
+     
 function playSound(e) {
-  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`); //тут ми беремо всі енти для дата кі ауоді
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`); 
   const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
-  if (!audio) return; //зупиняє цю функцію для всіх
-  audio.currentTime = 0; //повертає на початок
+  if (!audio) return; 
+  audio.currentTime = 0; 
   audio.play();
 //   console.log(key);
   key.classList.add(`playing`);
 }
 function remaoveTransition(e) {
-  if (e.propertyName !== `transform`) return; //пропускає якщо не змінено
+  if (e.propertyName !== `transform`) return; 
   this.classList.remove(`playing`);
 }
 const keys = document.querySelectorAll(`.key`);
@@ -28,7 +28,7 @@ let audio = document.getElementsByClassName("aud");
 for(let i = 0; i < button.length; i++) {
     button[i].addEventListener("click", function() {
         let id = this.id;
-        // button[i].classList.add(`playing`);
+         button[i].classList.add(`playing`);
     for(let i = 0; i < button.length; i++) {
         if(audio[i].id  === id) {
         audio[i].currentTime = 0;
